53ffdb3b7064b7b5ee5abb6cb2e23d99a4090b48
Mauricio Dulce <hola@mauriciodulce.com>
2020-11-19 13:43:35 -0500

    Update

--- tests/ApplicationTest.php
+++ tests/ApplicationTest.php
@@ -6,7 +6,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
- * @see https://github.com/MauricioDulce/framework
+ * @see https://github.com/wordplate/framework
  */
 
 declare(strict_types=1);
